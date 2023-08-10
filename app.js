f5a202d93efab1403eb605d858f6bd55f99ce7c3
Thamaraikannan <120456321+IT-Thamarai-demo@users.noreply.github.com>
2023-08-10 19:07:47 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,106 +1,14 @@
-intervalid = null;
-function butt() {
-    for (i = 0; i <= 60; i++) {
-        $('.extra').append(`<button  class="a${i}" id="btn${i}">${i}</button>`)
-        if (i % 2 == 0) {
-            $(`#btn${i}`).css({ "position": "absolute" })
-            $(`#btn${i}`).css({ "left": 30 * i - 30 })
-            $(`#btn${i}`).css({ "margin-top": "0px" })
-        }
-        else {
-
-            $(`#btn${i}`).css({ "position": "absolute" })
-            $(`#btn${i}`).css({ "left": 30 * i - 30 })
-            $(`#btn${i}`).css({ "margin-top": "500px" })
-
-        }
-    }
-
-
-}
-x = 0;
-function deck() {
-
-    if (x % 2 == 0) {
-        margintopvalue =parseInt($(`#btn${x}`).css('margin-top'))
-        if (margintopvalue == 0) {
-            $(`#btn${x}`).animate({'margin-top': '500px'})
-
-        }
-        else {
-
-            $(`#btn${x}`).animate({'margin-top':'0px'});
-        }
-    }
-    else {
-        margintopvalue = parseInt($(`#btn${x}`).css('margin-top'))
-        if (margintopvalue == 500) {
-
-
-            $(`#btn${x}`).animate({'margin-top':'0px'});
-
-
-        }
-        else {
-
-            $(`#btn${x}`).animate({'margin-top': '500px'})
-        }
-    }
-    x++;
-
-
-
-    if (x > 60) {
-        x = 0;
-    }
-
-
-
-
+function  increment()
+{
+  x=parseInt(document.querySelector("#se").textContent);
+  c=x+1;
+  document.querySelector("#se").textContent=c;
 }
 
-setInterval(deck, 100)
-function demo() {
-
-    if (x % 2 !== 0) {
-        margintopvalue = parseInt($(`#btn${x}`).css('margin-top'))
-        if (margintopvalue == 0) {
-            $(`#btn${x}`).css('margin-top', '500px')
-
-        }
-        else {
-
-            $(`#btn${x}`).css('margin-top', 0)
-        }
-    }
-    else {
-        margintopvalue =parseInt($(`#btn${x}`).css('margin-top'))
-        if (margintopvalue == 500) {
-
-
-            $(`#btn${x}`).css('margin-top', '0px')
-
-
-        }
-        else {
-
-            $(`#btn${x}`).css('margin-top', '500px')
-        }
-    }
-    x++;
-
-
-
-    if (x > 60) {
-        x = 0;
-    }
-
-
-
-
-
-
-
-
+function  decrement()
+{
+  x=parseInt(document.querySelector("#se").textContent);
+  c=x-1;
+  document.querySelector("#se").textContent=c;
 }
-setInterval(demo ,100)
+
