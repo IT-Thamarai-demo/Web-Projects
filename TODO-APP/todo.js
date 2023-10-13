898c943ed66939312441f0bd1a3e6db8b12783fd
Thamaraikannan <120456321+IT-Thamarai-demo@users.noreply.github.com>
2023-10-13 16:36:03 +0530

    Update todo.js

--- TODO-APP/todo.js
+++ TODO-APP/todo.js
@@ -1,1 +1,32 @@
+const input=document.querySelector("input");
+const todo=document.querySelector(".main-todo");
+const buttons=document.querySelector(".buttons");
 
+
+
+
+
+function addTask() {
+    if(input.value=='')
+    {
+       alert("enter your task")
+    }
+    else
+    {
+    let list=document.createElement("li");
+    list.innerHTML=input.value+"<button onclick=remove(event)>Delete Task</button>";
+    todo.appendChild(list)
+    }
+    input.value=""
+}
+ 
+   
+
+
+    
+
+function remove(event)
+{
+event.target.parentElement.remove();
+
+}
