3f50ba9a91b0ac98902f17e661b8c5c756e980bf
Thamaraikannan <120456321+IT-Thamarai-demo@users.noreply.github.com>
2024-09-14 13:25:40 +0530

    Update script.js

--- Receipe/script.js
+++ Receipe/script.js
@@ -193,10 +193,12 @@ document.querySelector('.btn').addEventListener('click', function () {
 // Register Service Worker for offline support
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      console.log('ServiceWorker registration failed: ', err);
-    });
+    navigator.serviceWorker.register('./service-worker.js') // Adjust path to match the folder structure
+      .then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
   });
 }
+
